46b8a359501ccdf5f2d838afb551510a88c2247a
AJAYKUMAR B S <ajaykumarbirursathish@gmail.com>
2024-11-17 15:17:00 +0530

    Create The Food Ordering Project Components like HEader, Main, RestaurantCard Component

--- Episode-04/Coding/app.js
+++ Episode-04/Coding/app.js
@@ -23,7 +23,7 @@ const Header = () => {
       <div className="logoContainer">
         <img
           className="main_Logo"
-          src="https://marketplace.canva.com/EAFaFUz4aKo/2/0/1600w/canva-yellow-abstract-cooking-fire-free-logo-JmYWTjUsE-Q.jpg"
+          src="https://img.freepik.com/free-vector/food-drink-hand-drawn-flat-healthy-food-logo_23-2149632253.jpg?semt=ais_hybrid"
         ></img>
       </div>
       <nav className="navContainer">
@@ -39,10 +39,55 @@ const Header = () => {
   );
 };
 
+const RestaurantCard = () => {
+  return (
+    <div className="restaurantCardContainer">
+      <div className="restaurantCardIMGContainer">
+        <img
+          className="restaurantCardIMG"
+          alt="restaurantCardIMG"
+          src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/yeednzto1jusnnhswke0"
+        ></img>
+      </div>
+      <div className="restaurantCardINFOContainer">
+        <h3>RestaurantName</h3>
+        <div className="ratingAndDeliveryTimeContainer">
+          <p>4.3</p>
+          <p>20 min</p>
+        </div>
+        <p className="cuisines">
+          Biryani, Chinese, Indian, Kebabs, Tandoor Mehdipatnam
+        </p>
+      </div>
+    </div>
+  );
+};
+const Main = () => {
+  return (
+    <main className="mainContainer">
+      <div className="searchContainer"></div>
+      <div className="restaurantContainer">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </main>
+  );
+};
 const AppLayout = () => {
   return (
     <div className="appLayoutContainer">
       <Header />
+      <Main />
     </div>
   );
 };
