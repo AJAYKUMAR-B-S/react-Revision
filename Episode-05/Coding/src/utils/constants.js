bee72574850e645ac89ceb543434f871aa6ba150
AJAYKUMAR B S <ajaykumarbirursathish@gmail.com>
2024-12-01 21:27:42 +0530

    Create  Constants to use across the projects

---
+++ Episode-05/Coding/src/utils/constants.js
@@ -0,0 +1,5 @@
+export const APP_LOGO_URL =
+  " https://img.freepik.com/free-vector/food-drink-hand-drawn-flat-healthy-food-logo_23-2149632253.jpg?semt=ais_hybrid";
+
+export const RESTAURANTCARD_RECIPE_IMG_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
